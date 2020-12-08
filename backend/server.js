7c7c41ce396c870c8ebced34a3b74268dd5c0198
Janice-M <janiceink001@gmail.com>
2020-12-08 23:54:55 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('This is my server response!');
+    res.end('This that  Jan response!');
 });
 
 server.listen(process.env.PORT || 3000);
