85c6a22335b9bc52b7060aadffa46b98af5ab7f3
Janice-M <janiceink001@gmail.com>
2020-12-14 23:50:04 +0300

    backend

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,6 @@ const app = express();
 
 
 app.use((req,res)=> {
-    res.json({message: 'Hey Rafiki, your message was successful'})
+    res.json({message: 'Hey Rafiki yangu, your message was successful'})
 })
 module.exports = app;
