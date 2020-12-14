fcf44129a8833de1e59350e05d548e892e8737d8
Janice-M <janiceink001@gmail.com>
2020-12-15 00:23:38 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -42,5 +42,6 @@ const server = http.createServer(app);
 server.on('error', errorHandler);
 server.on('listening', () => {
     const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
 
 });
