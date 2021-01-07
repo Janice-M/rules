c552fe932d6d1d1222219afb9fbc530f1219b604
Janice-M <janiceink001@gmail.com>
2021-01-07 22:18:57 +0300

    server#

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use((req, res, next) => {
-    console.log('Response sent successfully!');
+    console.log('Response sent successfully');
 });
 
 
