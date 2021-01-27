3c83af310827e62ad37ff03d2f5c49a00e1369d9
Janice-M <janiceink001@gmail.com>
2021-01-27 23:58:06 +0300

    error handling

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-    console.log('Request received');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.status(200);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Your request was successful' });
-    next();
-});
-app.use((req, res, next) => {
-    console.log('Response sent successfully');
-});
 
 
 module.exports = app;
