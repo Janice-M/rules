cd9379153be4f460fe60ecad5241a308f1c0ada4
Janice-M <janiceink001@gmail.com>
2021-01-28 00:02:50 +0300

    feeding my end point

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,11 @@ app.use('/api/stuff',(req, res, next) =>{
     const stuff = [
         {
             _id: 'oeihfzeoi',
-            
+            title : 'my first thing',
+            description : 'info about my first thing',
+            imageUrl : '',
+            price : 4900,
+            userId : 'quack'
         }
     ]
 });
