63ec1744f505e5f3456902b574fb2d77e57a8caa
Janice-M <janiceink001@gmail.com>
2021-01-28 00:13:14 +0300

    cors handling

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const app = express();
 app.use ((req, res, next,)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Acess-Control-Allow-Headers', 'Origin, X-Requeste-With, Content, Accept,Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH,OPTIONS');
+
+    next();
 })
 
 app.use('/api/stuff',(req, res, next) =>{
